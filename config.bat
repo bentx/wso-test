2caf4565fbe7c4d9537b11ac96ca67021a07ff28
bensonxavieretx <bensonxaviertx@gmail.com>
2020-06-23 13:10:41 +0530

    simply

--- config.bat
+++ config.bat
@@ -1,17 +1,11 @@
 
-echo working
 
-
-echo $(which apimcli )
 apimcli  version
 
-rd /s /q "$HOME/.wso2apictl"
+
 
 echo 'setting up dev environment'
-apimcli add-env -e dev \
-                    --registration https://dev.apim.wso2.com/client-registration/v0.15/register \
-                    --apim https://dev.apim.wso2.com \
-                    --token https://dev.apim.wso2.com/token \
-                    --admin https://dev.apim.wso2.com/api/am/admin/v0.15 \
-                    --api_list https://dev.apim.wso2.com/api/am/publisher/v0.15/apis \
-                    --app_list https://dev.apim.wso2.com/api/am/store/v0.15/applications
+
+apimcli add-env -n dev \ --registration https://localhost:9443/client-registration/v0.14/register \ --apim https://localhost:9443 \ --token https://localhost:8243/token \ --import-export https://localhost:9443/api-import-export-2.5.0-v1 \ --admin https://localhost:9443/api/am/admin/v0.14 \ --api_list https://localhost:9443/api/am/publisher/v0.14/apis \ --app_list https://localhost:9443/api/am/store/v0.14/applications
+
+apimcli add-env -n prod \ --registration https://localhost:9444/client-registration/v0.14/register \ --apim https://localhost:9444 \ --token https://localhost:8244/token \ --import-export https://localhost:9444/api-import-export-2.5.0-v1 \ --admin https://localhost:9444/api/am/admin/v0.14 \ --api_list https://localhost:9444/api/am/publisher/v0.14/apis \ --app_list https://localhost:9444/api/am/store/v0.14/applications
